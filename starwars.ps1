97be4350f0f5d90c735648dfb10507cfce3fe25b
Joe Shoulak <joepshoulak2@gmail.com>
2023-06-25 20:36:57 -0500

    added wifipassword payload

--- starwars.ps1
+++
@@ -1,7 +0,0 @@
-$TelnetStartedDisabled = ((Get-WindowsOptionalFeature -Online -FeatureName TelnetClient).State -ne "Enabled")
-
-if ($TelnetStartedDisabled) { Enable-WindowsOptionalFeature -Online -FeatureName TelnetClient }
-
-telnet towel.blinkenlights.nl
-
-if ($TelnetStartedDisabled) { Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient }
